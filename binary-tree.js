f83326516ab68ba8b1b468a047b393b600c0b66b
Nico Martinucci <domenico.martinucci@gmail.com>
2023-02-08 11:38:18 -0800

    Add binary tree node methods

--- binary-tree.js
+++ binary-tree.js
@@ -13,19 +13,39 @@ class BinaryTreeNode {
    * incomplete node-- that is, the length of the shortest path from the root to
    * a node with less than two children. */
   minDepthToIncompleteNode() {
+    // base case
+    if (!this.left || !this.right) return 1;
 
+    return 1 + Math.min(
+      this.left.minDepthToIncompleteNode(),
+      this.right.minDepthToIncompleteNode()
+    )
   }
 
   /** maxDepth(): return the maximum depth from the invoking node -- that is,
    * the length of the longest path from the invoking node to a leaf. */
-  maxDepth() {
+  maxDepth(depth = 1) {
+    // base case
+    if (!this.left && !this.right) return depth;
 
+    const leftDepth = this.left ? this.left.maxDepth(depth + 1) : 0;
+    const rightDepth = this.right ? this.right.maxDepth(depth + 1) : 0;
+
+    return Math.max(leftDepth, rightDepth);
   }
 
   /** minDepth(): return the minimum depth from the invoking node -- that is,
    * the length of the shortest path from the invoking node to a leaf. */
-   minDepth() {
+  minDepth() {
+    // base case
+    if (!this.left && !this.right) return 1;
+    if (!this.left) return 1 + this.right.minDepth();
+    if (!this.right) return 1 + this.left.minDepth();
 
+    // const leftDepth = this.left ? this.left.minDepth() : Infinity;
+    // const rightDepth = this.right ? this.right.minDepth() : Infinity;
+
+    return 1 + Math.min(this.left.minDepth(), this.right.minDepth());
   }
 }
 
@@ -41,7 +61,9 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   minDepthToIncompleteNode() {
+    if (!this.root) return 0;
 
+    return this.root.minDepthToIncompleteNode();
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
@@ -50,7 +72,9 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   maxDepth() {
+    if (!this.root) return 0;
 
+    return this.root.maxDepth();
   }
 
   /** minDepth(): return the minimum depth of the tree -- that is,
@@ -59,7 +83,9 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   minDepth() {
+    if (!this.root) return 0;
 
+    return this.root.minDepth();
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
